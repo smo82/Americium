301075bfc5f8ec6d33aae24f3a7fcdf1defc4afd
Simeon Simeonov <smo@gbg.bg>
2013-05-16 00:26:09 +0300

    Rebuild solution
    Rebuild solution because the solution file was missing

--- Hangman-7/HangmanGameTest/Properties/AssemblyInfo.cs
+++ Hangman-7/HangmanGameTest/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae702e15-efd7-439a-b0f1-1bb9f6f24774")]
+[assembly: Guid("c685e3f9-c3e0-4989-983f-a1b7e3f7bd16")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
